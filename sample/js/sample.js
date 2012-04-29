b722a2dac92a93dfa96837aa2851d6c63b30c325
Tanjo, Hiroyuki <expheno@gmail.com>
2012-04-29 18:20:44 +0900

    Improve inheritance.

--- sample/js/sample.js
+++ sample/js/sample.js
@@ -1,21 +1,23 @@
-(function($) {
-	
-	$(function() {
-		
-		$('.toggle .trigger a').togglePanel();
-		$('.smoothToggle .trigger a').smoothTogglePanel();
-		
-		$('a.closeAll').click(function(event) {
-			$('.smoothToggle .trigger a').smoothTogglePanel('close');
-			event.preventDefault();
-		});
-		$('a.close01').click(function(event) {
-			$('.smoothToggle .trigger a').eq(0).smoothTogglePanel('close', [function(){alert('closed!');}]);
-			event.preventDefault();
-		});
-		
-	});
-	
+(function ($) {
+
+    $(function () {
+
+        $('.toggle .trigger a').togglePanel();
+        $('.smoothToggle .trigger a').smoothTogglePanel();
+
+        $('a.closeAll').click(function (event) {
+            $('.smoothToggle .trigger a').smoothTogglePanel('close');
+            event.preventDefault();
+        });
+        $('a.close01').click(function (event) {
+            $('.smoothToggle .trigger a').eq(0).smoothTogglePanel('close', [function () {
+                alert('closed!');
+            }]);
+            event.preventDefault();
+        });
+
+    });
+
 })(jQuery);
 
 /* 
@@ -23,47 +25,44 @@
  * 
  * Toggle linked element.
  */
-(function($) {
-	
-	$.plugin({
-		name: 'togglePanel',
-		
-		defaults: {
-			target: null
-		},
-		
-		methods: {
-			initialize: function() {
-				var plugin = this;
-				this.trigger = this.element;
-				this.target = this.settings.target || $(this.trigger.attr('href'));
-				
-				//this.target.hide();
-				
-				this.trigger.click(function(event) {
-					if (plugin.target.is(':hidden')) {
-						plugin.open();
-					}
-					else {
-						plugin.close();
-					}
-					event.preventDefault();
-				});
-			},
-			
-			open: function() {
-				this.target.show();
-			},
-			
-			close: function() {
-				this.target.hide();
-			},
-			
-			trigger: null,
-			target: null
-		}
-	});
-	
+(function ($) {
+
+    $.plugin({
+        name: 'togglePanel',
+
+        defaults: {
+            target: null
+        },
+
+        methods: {
+            initialize: function () {
+                var self = this;
+                this.trigger = this.$element;
+                this.target = this.settings.target || $(this.trigger.attr('href'));
+
+                //this.target.hide();
+
+                this.trigger.click(function (event) {
+                    if (self.target.is(':hidden')) {
+                        self.open();
+                    }
+                    else {
+                        self.close();
+                    }
+                    event.preventDefault();
+                });
+            },
+
+            open: function () {
+                this.target.show();
+            },
+
+            close: function () {
+                this.target.hide();
+            }
+        }
+    });
+
 })(jQuery);
 
 /* 
@@ -71,25 +70,25 @@
  * 
  * Toggle linked element with animation.
  */
-(function($) {
-	
-	$.plugin({
-		name: 'smoothTogglePanel',
-		parent: 'togglePanel',
-		
-		defaults: {
-			duration: 200
-		},
-		
-		methods: {
-			open: function(callback) {
-				this.target.slideDown(this.settings.duration, callback);
-			},
-			
-			close: function(callback) {
-				this.target.slideUp(this.settings.duration, callback);
-			}
-		}
-	});
-	
+(function ($) {
+
+    $.plugin({
+        name: 'smoothTogglePanel',
+        parent: 'togglePanel',
+
+        defaults: {
+            duration: 200
+        },
+
+        methods: {
+            open: function (callback) {
+                this.target.slideDown(this.settings.duration, callback);
+            },
+
+            close: function (callback) {
+                this.target.slideUp(this.settings.duration, callback);
+            }
+        }
+    });
+
 })(jQuery);
--- source/jquery.plugin.js
+++ source/jquery.plugin.js
@@ -1,45 +1,46 @@
-(function($) {
-	
-	$.plugin = function(options) {
-		
-		// Definition
-		var name = options.name,
-			parent = options.parent,
-			defaults = options.defaults,
-			methods = options.methods;
-		
-		// Constructor
-		var Plugin = $[name] = function(element, options) {
-			this.element = $(element);
-			this.settings = $.extend({}, Plugin.defaults, options);
-			this.initialize();
-		};
-		
-		// Static members
-		if (parent) {
-			Plugin.defaults = $.extend(true, {}, $[parent].defaults);
-		}
-		Plugin.defaults = $.extend(Plugin.defaults, defaults);
-		
-		// Methods
-		if (parent) {
-			Plugin.prototype = new $[parent]();
-		}
-		Plugin.prototype = $.extend(Plugin.prototype, methods);
-		
-		// Interface
-		$.fn[name] = function(options, args) {
-			return this.each(function() {
-				var instance = $(this).data(name);
-				if (instance == undefined) {
-					instance = new $[name](this, options);
-					$(this).data(name, instance);
-				}
-				else if (typeof options == 'string') {
-					instance[options].apply(instance, args);
-				}
-			});
-		};
-	};
-	
-})(jQuery);
+(function ($) {
+
+    $.plugin = function (options) {
+
+        // Definition
+        var name = options.name,
+            parent = options.parent,
+            defaults = options.defaults,
+            methods = options.methods;
+
+        // Constructor
+        var Plugin = $[name] = function (element, options) {
+            this.$element = $(element);
+            this.settings = $.extend({}, $.fn[name].defaults, options);
+            this.initialize();
+        };
+
+        // Inheritance
+        if (parent) {
+            defaults = $.extend(true, {}, $.fn[parent].defaults, defaults);
+            Plugin.prototype = new $[parent]();
+            Plugin.prototype.constructor = Plugin;
+        }
+
+        // Methods
+        Plugin.prototype = $.extend(Plugin.prototype, methods);
+
+        // Interface
+        $.fn[name] = function (options, args) {
+            return this.each(function () {
+                var instance = $.data(this, name);
+                if (!instance) {
+                    instance = new $[name](this, options);
+                    $.data(this, name, instance);
+                }
+                else if (typeof options === 'string') {
+                    args = args || [];
+                    instance[options].apply(instance, args);
+                }
+            });
+        };
+        $.fn[name].defaults = defaults;
+
+    };
+
+}(jQuery));
